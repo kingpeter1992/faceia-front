c5ef41b152f9da12578634864e4675508b1bf282
kingpeter1992 <kingkapeta@gmail.com>
2026-06-20 18:42:30 +0200

    deployement

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,14 +1,14 @@
 export const environment = {
 
- production: false,
-  BASIC_URL: "http://127.0.0.1:8080/api/",
+  /*
+production: false,
+ BASIC_URL: "http://127.0.0.1:8080/api/",
+*/
 
 
-   /*
+  production: true,
 
-    production: true,
+  BASIC_URL: "https://faceia-back.onrender.com/api/",
 
-  BASIC_URL: "https://back-u-learning-2026.onrender.com/api/",
 
-   */
 };
